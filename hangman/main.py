37a7727606fae3dd733b0e8a02a95628edbb71ac
Active Profi <atosdo@live.com>
2024-06-15 07:18:51 +0000

    first working version of the game (MVP)

--- hangman/main.py
+++ hangman/main.py
@@ -6,37 +6,104 @@ ATTEMPTS = len(pics.FRAME)
 WORDS_FILE_NAME = "nounlist.txt"
 
 
-def greetings() -> None:
+def show_welcome_message() -> None:
     msg = f"""Welcome to hangman game!
-
-You must guess all letters in random word. Attemts number is {ATTEMPTS}.
-The game lasts until you guess the word or until there are attempts left."""
+You must guess all letters in random word. Attemt count is {ATTEMPTS}.
+The game lasts until you guess the word or until there are attempts left.
+"""
 
     print(msg)
 
 
-def display_game_status():
-    raise NotImplementedError
+def show_game_status(
+    attempt_count: int, player_word: str, guess_chars: list[str]
+) -> None:
+    print(
+        f"""\nAttempt №{attempt_count}
+Your word is => {player_word}
+Letters you've guessed => {guess_chars}
+
+{pics.FRAME[attempt_count - 1]}\n"""
+    )
 
 
-def get_user_input() -> str:
-    raise NotImplementedError
+def get_user_input(guess_chars: list[str]) -> str:
+    while True:
+        in_char = input("Please, enter the character: ")[0]
+
+        if not in_char.isalpha():
+            print("Enter a character, not something else")
+            continue
+
+        if in_char in guess_chars:
+            print("That's the symbol you've already guessed, try another.")
+            continue
+
+        return in_char
+
+
+def get_guessed_chars(random_word: str, entered_chars: list[str]) -> list[str]:
+    guessed_chars = [ch for ch in random_word if ch in entered_chars]
+    return guessed_chars
+
+
+def show_user_statistics(
+    attempt_count: int, entered_chars: list[str], player_word: str, random_word: str
+):
+    guessed_chars = get_guessed_chars(random_word, entered_chars)
+    placeholder = "-" * (35 + len(entered_chars * 3))
+
+    print(
+        f"""
+Your statictics:
+{placeholder}
+| The guess word was =>         {random_word}
+| The number of attempts was => {attempt_count}
+| Entered letters =>            {", ".join(entered_chars)}
+| Your word =>                  {player_word}
+| Guessed letters =>            {", ".join(guessed_chars)}
+{placeholder}
+"""
+    )
+
+
+def show_goodbye_message():
+    print("Thank you for playing our game. Have a good day!")
 
 
 def game():
-    greetings()
-    random_word = words.random_word_from_file(f"{os.getcwd()}{os.sep}{WORDS_FILE_NAME}")
-    guess_chars = ""
-    player_word = words.get_masked_word(random_word, guess_chars)
+    show_welcome_message()
+    game_count = 0
+    while True:
+        target_word = words.get_random_word(f"{os.getcwd()}{os.sep}{WORDS_FILE_NAME}")
+        entered_chars = []
+        guessed_word = words.get_masked_word(target_word, entered_chars)
+        attempt_count = 1
 
-    while random_word != player_word or ATTEMPTS:
-        display_game_status()
-        in_char = get_user_input()
+        while guessed_word != target_word and attempt_count <= ATTEMPTS:
+            show_game_status(attempt_count, guessed_word, entered_chars)
 
+            input_char = get_user_input(entered_chars)
+            entered_chars.append(input_char)
+            guessed_word = words.get_masked_word(target_word, entered_chars)
 
-def main():
-    game()
+            if input_char not in target_word:
+                if attempt_count == ATTEMPTS:
+                    break
+
+                attempt_count += 1
+
+        if target_word == guessed_word:
+            print("\nYou won the game! Congratulation! :)")
+        elif attempt_count == ATTEMPTS:
+            print("\nYou lost by running out of tries. :(")
+
+        show_user_statistics(attempt_count, entered_chars, guessed_word, target_word)
+
+        if input("Do you want one more game? y/n").lower()[0] == "n":
+            show_goodbye_message()
+            break
 
 
 if __name__ == "__main__":
-    main()
+    game()
--- hangman/words.py
+++ hangman/words.py
@@ -1,40 +1,26 @@
 import random
+import pics
 
-MASKED_CHAR = "-"
+MASKED_SYMBOL = "-"
+MIN_WORD_LEN = 4
+MAX_WORD_LEN = len(pics.FRAME)
 
 
-def random_word_from_file(filepath):
-    """Return random word from file
-
-    Args:
-        filepath (str): path to the file with words
-
-    Returns:
-        str: single word from file
-    """
+def get_random_word(filepath: str) -> str:
     with open(filepath, "r", encoding="utf-8") as file:
-        words = [ln for line in file if len(ln := line.strip()) > 4]
+        words = [
+            w
+            for line in file
+            if len(w := line.strip()) > MIN_WORD_LEN and len(w) <= MAX_WORD_LEN
+        ]
 
     word = words[random.randint(0, len(words))]
     return word
 
 
-def get_masked_word(random_word, guess_chars):
-    """Get masked word with masked char that corresponding to guess word
-
-    Args:
-        random_word (str): random word that computer wished
-        guess_chars (str): string with guess letters
-
-    Returns:
-        str: masked word
-    """
-
+def get_masked_word(target_word: str, entered_chars: list[str]) -> str:
     masked_word = "".join(
-        [char if char in guess_chars
-         else MASKED_CHAR
-         for char in random_word
-         ]
+        [char if char in entered_chars else MASKED_SYMBOL for char in target_word]
     )
 
     return masked_word
